f0cb187eefe2673f687a59def690fe94dd892885
Guo Yunhe <guoyunhebrave@gmail.com>
2014-07-17 23:45:43 +0800

    Windows: Migrate from Linux version

---
+++ DarkRoomWindows/src/darkroom/gui/AppWindow.java
@@ -0,0 +1,131 @@
+package darkroom.gui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+
+import javax.swing.JToggleButton;
+import javax.swing.WindowConstants;
+
+import darkroom.io.HostsIO;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class AppWindow {
+
+	private JFrame mainFrame;
+	private JToggleButton toggleButton;
+	private HostsIO hostsio;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppWindow window = new AppWindow();
+					window.mainFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		hostsio = new HostsIO();
+		
+		boolean isLocked = hostsio.readHosts();
+		
+		mainFrame = new JFrame();
+		mainFrame.setTitle("小黑屋/真的要来不及了！");
+		mainFrame.setBounds(20, 20, 300, 200);
+		mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainFrame.setResizable(false);
+		
+		mainFrame.getContentPane().setBackground(Color.WHITE);
+		mainFrame.getContentPane().setLayout(null);
+		
+		toggleButton = new JToggleButton("关进小黑屋");
+		toggleButton.setBounds(90, 60, 120, 40);
+		toggleButton.setFont(new Font("Dialog", Font.PLAIN, 16));
+		toggleButton.setFocusPainted(false);
+		toggleButton.setBackground(Color.BLACK);
+		toggleButton.setForeground(Color.WHITE);
+		toggleButton.setBorderPainted(false);
+		toggleButton.addMouseListener(listener);
+		mainFrame.getContentPane().add(toggleButton);
+		
+		// TODO Check if hosts is dark
+		if(isLocked) {
+			toggleButton.setSelected(true);
+			mainFrame.getContentPane().setBackground(Color.BLACK);
+			toggleButton.setForeground(Color.BLACK);
+			toggleButton.setText("救出小黑屋");
+		}
+	}
+	
+	MouseListener listener = new MouseListener(){
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			if(toggleButton.isSelected()) {
+				// Unselected -> Selected
+				// Light -> Dark
+				System.out.println("Dark!");
+				mainFrame.getContentPane().setBackground(Color.BLACK);
+				toggleButton.setForeground(Color.BLACK);
+				toggleButton.setText("救出小黑屋");
+				hostsio.writeHosts(true);
+			} else {
+				// Selected -> Unselected
+				// Dark -> Light
+				System.out.println("Light~");
+				mainFrame.getContentPane().setBackground(Color.WHITE);
+				toggleButton.setForeground(Color.WHITE);
+				toggleButton.setText("关进小黑屋");
+				hostsio.writeHosts(false);
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
+}
---
+++ DarkRoomWindows/src/darkroom/io/HostsIO.java
@@ -0,0 +1,189 @@
+package darkroom.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HostsIO {
+	private File hosts;
+	private BufferedReader bufHostsIn;
+	private BufferedWriter bufHostsOut;
+	private File temp;
+	private BufferedReader bufTempIn;
+	private BufferedWriter bufTempOut;
+	private File list;
+	private BufferedReader bufListIn;
+	
+	public HostsIO() {
+		init();
+	}
+	
+	private void init() {
+		// File objects
+		hosts = new File(System.getenv("windir")+"\\system32\\drivers\\etc\\hosts"); // use "hosts" when test
+										// avoid root permission and effects on your system 
+		list = new File("list");
+		// Create temp file
+		try {
+			temp = File.createTempFile("darkroom-host-", "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		temp.deleteOnExit();
+	}
+	
+	public boolean readHosts() {
+		boolean isLocked = false;
+		
+		String line = "";
+		
+		// Buffered reader and writer
+		try {
+			bufHostsIn = new BufferedReader(new FileReader(hosts));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		while(true) {
+			// Read original hosts file
+			try {
+				line = bufHostsIn.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(line==null) {
+				break;
+			}
+			
+			if(line.startsWith("#DarkRoomStart")) {
+				isLocked = true;
+				break;
+			}
+		}
+		
+		// Close file IO
+		try {
+			bufHostsIn.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return isLocked;
+	}
+	
+	public void writeHosts(boolean lock) {
+		
+		boolean skip = false;
+		
+		String line = "";
+		
+		// Buffered reader and writer
+		try {
+			bufHostsIn = new BufferedReader(new FileReader(hosts));
+			bufTempOut = new BufferedWriter(new FileWriter(temp));
+			bufListIn = new BufferedReader(new FileReader(list));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		while(true) {
+			// Read original hosts file
+			try {
+				line = bufHostsIn.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(line==null) {
+				break;
+			}
+			
+			if(line.startsWith("#DarkRoomStart")) {
+				skip = true;
+			}
+			
+			if(!skip) {
+				try {
+					bufTempOut.write(line);
+					bufTempOut.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(line.startsWith("#DarkRoomEnd")) {
+				skip = false;
+			}	
+		}
+		
+		// Add lock
+		if(lock) {
+			while(true) {
+				// Read block list file
+				try {
+					line = bufListIn.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if(line==null) {
+					break;
+				}
+				// Write into temp hosts file
+				try {
+					bufTempOut.write(line);
+					bufTempOut.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// Close file IO
+		try {
+			bufListIn.close();
+			bufHostsIn.close();
+			bufTempOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Buffered reader and writer
+		try {
+			bufHostsOut = new BufferedWriter(new FileWriter(hosts));
+			bufTempIn = new BufferedReader(new FileReader(temp));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Transfer content of temp file to hosts
+		line = null;
+		while(true) {
+			// Read temp file
+			try {
+				line = bufTempIn.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// Write into hosts file
+			System.out.println(line);
+			if(line==null) {
+				break;
+			}
+			try {
+				bufHostsOut.write(line);
+				bufHostsOut.newLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// Close file IO
+		try {
+			bufHostsOut.close();
+			bufTempIn.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
